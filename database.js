eb04491c9d83af82d9b2e7913c18d40472c12b9a
Pankajlamgria <125663310+Pankajlamgria@users.noreply.github.com>
2023-07-20 11:03:27 +0530

    connected by mongoatlas

--- database.js
+++ database.js
@@ -1,7 +1,16 @@
 const mongoose=require("mongoose");
-const db=process.env.DATABASE;
+const db2=`${process.env.DATABASE}`;
+// function connectdatabase() {
+//     // mongoose.connect('mongodb://0.0.0.0:27017/blog');
+//     mongoose.connect(`${db}`);
+// }
+// module.exports=connectdatabase;
 function connectdatabase() {
-    // mongoose.connect('mongodb://0.0.0.0:27017/blog');
-    mongoose.connect(`${db}`);
+    mongoose.connect(db2,{
+        // useCreateIndex:true,
+        // useFindAndModify:false,
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }).then(()=>console.log("connection started")).catch((error)=>console.log(error.message));
 }
 module.exports=connectdatabase;
